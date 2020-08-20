521782b8b56a046e7a0e0bf2554dde672c4e2b35
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-08-20 10:09:59 +0530

    added migration for updated

--- db/migrate/20200820043619_add_time_stamps_to_questions.rb
+++ db/migrate/20200820043619_add_time_stamps_to_questions.rb
@@ -1,4 +1,6 @@
 class AddTimeStampsToQuestions < ActiveRecord::Migration[6.0]
   def change
+    add_column :questions, :created_at, :datetime
+    add_column :questions, :updated_at, :datetime
   end
 end
