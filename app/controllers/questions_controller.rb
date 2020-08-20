04c66293740e85ae64df451deddd49e742971310
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-08-20 16:40:23 +0530

    CRUD operation using view and controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,53 @@
+class QuestionsController < ApplicationController
+     before_action :set_question, only: [:show, :edit, :update, :destroy] 
+    def index
+        @questions = Question.all
+    end
+    def new
+        @question = Question.new
+    end 
+    def create 
+        @question = Question.new(question_params)
+        respond_to do |format|
+            if @question.save
+                format.html { redirect_to @question, notice: 'Question created!'}
+            else
+                format.html { render :new}
+            end
+        end
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end 
+
+    def destroy
+        @question.destroy
+        respond_to do |format|
+            format.html { redirect_to @question, notice: 'Question Deleted' }
+        end
+    end 
+
+    def update
+        respond_to do |format|
+            if @question.update(question_params)
+                format.html { redirect_to @question, notice: 'Question updated!'}
+            else
+                format.html { render :edit}
+            end
+        end
+    end 
+
+    def set_question
+        @question = Question.find(params[:id])
+    end 
+    def question_params
+        params.require(:question).permit(:title, :description)
+    end 
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :questions
+  root to: 'questions#index'
 end
