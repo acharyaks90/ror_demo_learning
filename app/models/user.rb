9bfa3f5e544d40bf81dbcd016b4620f799723831
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-08-31 15:00:37 +0530

    Added columns to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :questions
-    validates :username, presence: true, uniqueness: {case_sensitive:true}, length: { minimum: 3, maximum: 5}
+    validates :username, presence: true, uniqueness: {case_sensitive:true}, length: { minimum: 5, maximum: 12}
+    validates :email, presence: true, uniqueness: {case_sensitive:false}, length: { minmum: 4, maximum: 20}
+    validates :password, presence: true, length: { minimum: 8, maximum: 16}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_095745) do
+ActiveRecord::Schema.define(version: 2020_08_31_055148) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_08_28_095745) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password"
   end
 
 end
