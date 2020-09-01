2c406dae26e5504e9d6d5174ac0293dbc0f7131a
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-09-01 11:07:33 +0530

    has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: {case_sensitive:true}, length: { minimum: 5, maximum: 12}
     validates :email, presence: true, uniqueness: {case_sensitive:false}, length: { minmum: 4, maximum: 20}
     validates :password, presence: true, length: { minimum: 8, maximum: 16}
+    has_secure_password
 end
