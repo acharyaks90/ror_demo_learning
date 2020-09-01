b10475f50a2e281708d344db8b7276b03ff20ef7
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-09-01 10:38:30 +0530

    BCrypt use

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase}
     has_many :questions
     validates :username, presence: true, uniqueness: {case_sensitive:true}, length: { minimum: 5, maximum: 12}
     validates :email, presence: true, uniqueness: {case_sensitive:false}, length: { minmum: 4, maximum: 20}
