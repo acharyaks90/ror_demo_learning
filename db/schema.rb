0154bf50a1cd3f8cd9d120e2bd1f17934d19c41e
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-09-01 11:07:59 +0530

    password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_055148) do
+ActiveRecord::Schema.define(version: 2020_09_01_051033) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_055148) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
