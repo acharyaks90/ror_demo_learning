61def9ec109a3f33453720ff3f0e2647698bced9
Anil Suryavanshi <thakurinfotech@gmail.com>
2020-09-02 15:55:52 +0530

    Login and logout managed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'home/about'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'session#destroy'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :questions
   root to: 'questions#index'
