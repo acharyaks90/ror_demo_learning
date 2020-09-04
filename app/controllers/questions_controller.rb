98d0db86c6082d5c0a0c2ee1970d337837c9440b
Anil Kumar Suryavanshi <anil.kumar@engineer.ai>
2020-09-04 11:51:52 +0530

    Checking and restrict action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,10 @@
 class QuestionsController < ApplicationController
      before_action :set_question, only: [:show, :edit, :update, :destroy] 
     def index
+       
         @questions = Question.all
-    end
+        end
+    
     def new
         @question = Question.new
     end 
