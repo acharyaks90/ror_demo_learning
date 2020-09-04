c3cd75abb8a5538180039eb218e3548ee2be47c4
Anil Kumar Suryavanshi <anil.kumar@engineer.ai>
2020-09-04 13:00:55 +0530

    destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase}
-    has_many :questions
+    has_many :questions, dependent: :destroy
     validates :username, presence: true, uniqueness: {case_sensitive:true}, length: { minimum: 5, maximum: 12}
     validates :email, presence: true, uniqueness: {case_sensitive:false}, length: { minmum: 4, maximum: 20}
     validates :password, presence: true, length: { minimum: 8, maximum: 16}
