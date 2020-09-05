adf7bbce22aa41f345a9090d32dffdcd0306939a
Anil Kumar Suryavanshi <anil.kumar@engineer.ai>
2020-09-05 12:05:34 +0530

    Added a column admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_051033) do
+ActiveRecord::Schema.define(version: 2020_09_05_062946) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_051033) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
